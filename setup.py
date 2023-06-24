bd1992efd71370a1d50d0d94089eb87dd6403d36
burtonrj <burtonrossj@gmail.com>
2023-06-24 18:13:40 +0100

    Updated toml to make sure the cpp files are included in the package

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
-from setuptools import setup, Extension
 from pybind11.setup_helpers import Pybind11Extension
+from setuptools import setup
 
 ext_modules = [
     Pybind11Extension(
         "cytotransform.logicle_ext",
         [
             "cytotransform/logicle_ext/FastLogicle.cpp",
-            "cytotransform/logicle_ext/Logicle.cpp"
-        ]
+            "cytotransform/logicle_ext/Logicle.cpp",
+        ],
     )
 ]
 
-setup(
-    name="cytotransform",
-    version="0.1.0",
-    ext_modules=ext_modules
-)
+setup(name="cytotransform", version="0.1.1", ext_modules=ext_modules)
