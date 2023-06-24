d09e0c0403d67d3266d743cc34bebe0ad22dfab6
burtonrj <burtonrossj@gmail.com>
2023-06-24 18:21:58 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from pybind11.setup_helpers import Pybind11Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 ext_modules = [
@@ -11,4 +11,14 @@ ext_modules = [
     )
 ]
 
-setup(name="cytotransform", version="0.1.1", ext_modules=ext_modules)
+setup(
+    name="cytotransform",
+    version="0.1.2",
+    author="Ross Burton",
+    author_email="burtonrossj@gmail.com",
+    ext_modules=ext_modules,
+    extras_require={"test": "pytest"},
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.11",
+)
